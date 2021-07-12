d699b36f2408a46dab0ec93eedfc660db9ab181b
Srikanth Talapadi <sri@sutara.org>
2021-07-13 00:03:09 +0530

    Updated build

--- scripts/copy_assets.py
+++ scripts/copy_assets.py
@@ -9,6 +9,9 @@ import shutil
 parser = argparse.ArgumentParser()
 parser.add_argument("course_dir", help="Course dir")
 parser.add_argument("asset_dir", help="Asset dir")
+parser.add_argument("--audio", action='store_true', help="Process audio")
+parser.add_argument("--images", action='store_true', help="Process images")
+parser.add_argument("--overwrite", action='store_true', help="overwrite existing files (default: do not overwrite)")
 args = parser.parse_args()
 
 for json_file in glob(args.course_dir + '/**/*.json', recursive=True):
@@ -16,8 +19,17 @@ for json_file in glob(args.course_dir + '/**/*.json', recursive=True):
         lesson = json.load(json_f)
         for row in lesson['rows']:
             for item in row:
-                if item.endswith('.mp3') and not os.path.isfile(os.path.dirname(json_file) + '/' + item):
-                    if os.path.isfile(args.asset_dir + '/' + item):
-                        shutil.copyfile(args.asset_dir + '/' + item, os.path.dirname(json_file) + '/' + item)
-                    else:
-                        print('No file: '+item)
+                if args.audio and item.endswith('.mp3'):
+                    if args.overwrite or not os.path.isfile(os.path.dirname(json_file) + '/' + item):
+                        if os.path.isfile(args.asset_dir + '/audio/' + item):
+                            shutil.copyfile(args.asset_dir + '/audio/' + item, os.path.dirname(json_file) + '/' + item)
+                        else:
+                            if not os.path.isfile(os.path.dirname(json_file) + '/' + item):
+                                print('No audio file: '+item)
+                if args.images and item.endswith('.png'):
+                    if args.overwrite or not os.path.isfile(os.path.dirname(json_file) + '/' + item):
+                        if os.path.isfile(args.asset_dir + '/images/' + item):
+                            shutil.copyfile(args.asset_dir + '/images/' + item, os.path.dirname(json_file) + '/' + item)
+                        else:
+                            if not os.path.isfile(os.path.dirname(json_file) + '/' + item): 
+                                print('No image file: '+item)
